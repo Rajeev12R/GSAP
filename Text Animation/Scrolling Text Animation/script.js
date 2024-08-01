89dce0f51a3dc4e20944a321af590bb678f30ec6
Rajeev Ranjan <rjranjan2112@gmail.com>
2024-08-01 19:43:58 +0530

    Scrolling Marque Text Animation

--- Text Animation/Scrolling Text Animation/script.js
+++ Text Animation/Scrolling Text Animation/script.js
@@ -1,3 +1,28 @@
-window.addEventListener("wheel", function (details) {
-    console.log(details.deltaY);
-})
+function scrollWheelMarqueText() {
+    window.addEventListener("wheel", function (details) {
+        if (details.deltaY > 0) {
+            gsap.to(".marque", {
+                transform: 'translateX(-200%)',
+                duration: 4,
+                repeat: -1,
+                ease: "none"
+            });
+            gsap.to(".marque img", {
+                rotate: 180
+            });
+        }
+        else {
+            gsap.to(".marque", {
+                transform: 'translateX(0%)',
+                duration: 4,
+                repeat: -1,
+                ease: "none"
+            });
+            gsap.to(".marque img", {
+                rotate: 0
+            });
+        }
+    })
+
+}
+scrollWheelMarqueText()
